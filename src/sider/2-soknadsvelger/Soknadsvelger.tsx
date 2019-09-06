4e94a086a4b895d777a0e24c04895e3b7d9b1d9d
My Thao Nguyen <mythng94@gmail.com>
2019-09-06 15:57:52 +0200

    STR: mellomlagring

--- src/sider/2-soknadsvelger/Soknadsvelger.tsx
+++ src/sider/2-soknadsvelger/Soknadsvelger.tsx
@@ -70,9 +70,14 @@ class Soknadsobjekter extends Component<MergedProps> {
             case "LOADING":
               return <Spinner style={{ backgroundColor: "white" }} />;
             case "RESULT":
-              let ettApentObjekt = soknader.soknadsobjekter.length + soknader.soknadslenker.length < 2;
-              let apentSoknadsobjekt = ettApentObjekt && soknader.soknadsobjekter.length === 1;
-              let apenSoknadslenke = ettApentObjekt && soknader.soknadslenker.length === 1;
+              let ettApentObjekt =
+                soknader.soknadsobjekter.length +
+                  soknader.soknadslenker.length <
+                2;
+              let apentSoknadsobjekt =
+                ettApentObjekt && soknader.soknadsobjekter.length === 1;
+              let apenSoknadslenke =
+                ettApentObjekt && soknader.soknadslenker.length === 1;
               return (
                 <>
                   {valgtUnderkategori && (
@@ -80,11 +85,19 @@ class Soknadsobjekter extends Component<MergedProps> {
                   )}
 
                   {soknader.soknadsobjekter.map((soknadsobjekt, id) => (
-                    <VisSoknadsobjekt key={id} soknadsobjekt={soknadsobjekt} apen={apentSoknadsobjekt} />
+                    <VisSoknadsobjekt
+                      key={id}
+                      soknadsobjekt={soknadsobjekt}
+                      apen={apentSoknadsobjekt}
+                    />
                   ))}
 
                   {soknader.soknadslenker.map((soknadslenke, id) => (
-                    <VisSoknadslenke key={id} soknadslenke={soknadslenke} apen={apenSoknadslenke} />
+                    <VisSoknadslenke
+                      key={id}
+                      soknadslenke={soknadslenke}
+                      apen={apenSoknadslenke}
+                    />
                   ))}
                 </>
               );
---
+++ src/utils/scroll.tsx
@@ -0,0 +1,38 @@
+export function scrollTilElement(
+  elementid?: string,
+  oppforsel: ScrollBehavior = "smooth"
+) {
+  let elementPos = 0;
+
+  if (typeof elementid !== "undefined") {
+    elementPos =
+      document.getElementById(elementid)!.getBoundingClientRect().top +
+      window.scrollY;
+  }
+
+  try {
+    window.scroll({
+      top: elementPos,
+      behavior: typeof oppforsel === "undefined" ? "smooth" : oppforsel
+    });
+  } catch (e) {
+    try {
+      if (typeof elementid === "undefined") {
+        elementid = "periodebanner";
+      }
+      document.getElementById(elementid)!.scrollIntoView();
+    } catch (e) {
+      console.log("Kunne ikke scrolle");
+    }
+  }
+}
+
+export function scrollTilNyttVedlegg(
+  elementid: string,
+  oppforsel: ScrollBehavior = "smooth",
+  isCheckedYet: boolean | undefined
+) {
+  isCheckedYet === undefined
+    ? document.getElementById(elementid)!.scrollIntoView()
+    : console.log("cannot scroll yo");
+}
