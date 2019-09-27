9a82aa5c9b8d4c2562433b5f91517a93895e9f48
Sissel Fladby <sissel.fladby@nav.no>
2019-09-27 09:23:04 +0200

    Tester ut fjerning av filesaver

--- src/sider/4-avslutning/steg/utils/pdf.ts
+++ src/sider/4-avslutning/steg/utils/pdf.ts
@@ -1,5 +1,4 @@
 import { b64toBlob } from "./blob";
-import FileSaver from "file-saver";
 import { LocalePDFObjekt } from "../../../../typer/sprak";
 
 export const hentPDFurl = (
@@ -34,12 +33,45 @@ export const hentPDFobjekt = (
   }
 };
 
+/*
 export const lastNedFil = (url: string, tittel: string, filtype: string) => {
   console.log(`Laster ned ${tittel}`);
   FileSaver.saveAs(url, `${tittel}.${filtype}`);
 };
 
+^ Tester det å fjerne fileSaver for nedlastinger mot sanity, i og med at vi tror vi opplever noe problemer med dette.
+*/
+
 export const lastNedFilBase64 = (base64: string, tittel: string, filtype: string) => {
   console.log(`Laster ned ${tittel}`);
-  FileSaver.saveAs(b64toBlob(base64), `${tittel}.${filtype}`);
+  const url = window.URL.createObjectURL(new Blob([b64toBlob(base64)]));
+  automatiskNedlasting(url, tittel, filtype);
+  window.URL.revokeObjectURL(url);
 };
+
+export async function lastNedFil(filUrl: string, tittel: string, filtype: string) {
+  try {
+    const response = await fetch(filUrl);
+    if (!response.ok) {
+      console.error(`Network response was not ok ${response.status} ${response.statusText}`);
+    }
+    const myBlob = await response.blob();
+    const url = window.URL.createObjectURL(new Blob([myBlob]));
+    automatiskNedlasting(url, tittel, filtype);
+    window.URL.revokeObjectURL(url);
+  } catch (error) {
+    console.log("There has been a problem with your fetch operation: ", error.message);
+  }
+
+}
+
+const automatiskNedlasting = (url: string, tittel: string, filtype: string) => {
+  const link = document.createElement("a");
+  link.href = url;
+  link.setAttribute("download", `${tittel}.${filtype}`);
+  document.body.appendChild(link);
+  link.click();
+  if (link.parentNode) {
+    link.parentNode.removeChild(link);
+  }
+}
